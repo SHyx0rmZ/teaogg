4ae7df4d641e152e6bf39e0ad2dea01ff980a0df
Sage D. Layton <Goldenerkreiger@hotmail.de>
2013-10-28 22:54:50 +0100

    SDL_Window & SDL_Renderer added

--- TEAOGG/main.cpp
+++ TEAOGG/main.cpp
@@ -2,7 +2,37 @@
 
 int main(int argc, char *argv[])
 {
+    const char *title     = "The Extreme Adventure Of Grandpa George";
+    const unsigned width  = 640;
+    const unsigned height = 480;
+
+
     SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN);
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_TARGETTEXTURE);
+
+    SDL_Event event;
+    SDL_bool alive = SDL_TRUE;
+
+    while(alive)
+    {
+        while(SDL_PollEvent(&event))
+        {
+            if(event.type == SDL_QUIT)
+            {
+                alive = SDL_FALSE;
+            }
+        }
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+
+    SDL_DestroyWindow(window);
 
     SDL_Quit();
 
